cf469c20a150c488a594ab56e0fee01210a734e4
Joane Alves <joanealves2011@gmail.com>
2025-04-29 10:51:39 -0300

    Alter Color button whatsapp

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -12,7 +12,7 @@ const WhatsAppButton = () => {
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50"
     >
-      <div className="backdrop-blur-md bg-white/30 hover:bg-white/40 text-black shadow-lg p-4 rounded-full transition-all duration-300 cursor-pointer">
+      <div className="backdrop-blur-md bg-white/30 hover:bg-white/40 text-white shadow-lg p-4 rounded-full transition-all duration-300 cursor-pointer">
         <MessageCircle className="w-6 h-6" />
       </div>
     </a>
